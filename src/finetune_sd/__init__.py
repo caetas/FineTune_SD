29c878fd51c52496ec2c5e3cd13d8e94684681c7
caetas <up201705031@fe.up.pt>
2023-09-07 22:50:48 +0100

    Setup cookiecutter

---
+++ src/finetune_sd/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for finetune_sd.
+
+    Fine-Tuning a Stable Diffusion model on a limited amount of VRAM.
+"""
+from version import __version__
+
+__author__ = "Francisco Caetano"
---
+++ test/test_import_utils.py
@@ -0,0 +1,4 @@
+from finetune_sd.utils.import_utils import is_installed
+
+def test_is_installed():
+    assert is_installed("numpy")
