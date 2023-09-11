bb4c51e840131db7c22e1b310c0e5ebe47a36ef0
caetas <up201705031@fe.up.pt>
2023-09-12 00:57:01 +0100

    New results

--- src/finetune_sd/control_execute.sh
+++ src/finetune_sd/control_execute.sh
@@ -10,13 +10,20 @@ accelerate launch train_controlnet.py \
  --resolution=512 \
  --learning_rate=1e-5 \
  --max_train_steps=10000 \
+ --checkpointing_steps=2000 \
  --validation_image "./cond_0.png" \
  --validation_prompt "red pokemon with black wings" \
- --train_batch_size=1 \
+ --train_batch_size=4 \
  --image_column="image" \
  --caption_column="text" \
  --conditioning_image_column='conditioning_image' \
- --gradient_accumulation_steps=4 \
+ --gradient_accumulation_steps=1 \
  --gradient_checkpointing \
  --use_8bit_adam \
  --enable_xformers_memory_efficient_attention \
+
+git add --all
+git commit -m "New results"
+git push
+
+poweroff
--- src/finetune_sd/train_controlnet.py
+++ src/finetune_sd/train_controlnet.py
@@ -543,7 +543,7 @@ def parse_args(input_args=None):
     parser.add_argument(
         "--validation_steps",
         type=int,
-        default=500,
+        default=1000,
         help=(
             "Run validation every X steps. Validation consists of running the prompt"
             " `args.validation_prompt` multiple times: `args.num_validation_images`"
